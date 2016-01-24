d3c7f7ea81f1381d59c7956d0e75a88e8c3a0944
Eduardo Agostini <edu.agostini@gmail.com>
2016-01-25 09:50:48 +1300

    PSR
    Applying *PSR* standards.

--- src/Flush.php
+++ src/Flush.php
@@ -6,8 +6,8 @@ namespace BrownPaperBag;
  * Class Flush
  * @package BrownPaperBag
  */
-class Flush{
-
+class Flush
+{
     /**
      * @const string
      */
@@ -38,16 +38,13 @@ class Flush{
      * @param string $contentType
      * @param bool $enableLimbo
      */
-    public function __construct($contentType = '', $enableLimbo = false){
-
-        if($contentType){
-
+    public function __construct($contentType = '', $enableLimbo = false)
+    {
+        if ($contentType) {
             $this->setContentType($contentType);
-
         }
 
         $this->setLimboEnabled($enableLimbo);
-
     }
 
     /**
@@ -55,41 +52,36 @@ class Flush{
      * @param string $value 
      * @return Flush
      */
-    public function addHeader($key, $value){
-
+    public function addHeader($key, $value)
+    {
         $this->headers[$key] = $value;
 
         return $this;
-
     }
 
     /**
      * @param string $key
      * @return Flush
      */
-    private function applyHeader($key){
-
+    private function applyHeader($key)
+    {
         $value = $this->getHeaderAndRemove($key);
 
         header(implode(': ', array($key, $value)));
 
         return $this;
-
     }
 
     /**
      * @return Flush
      */
-    private function applyHeaders(){
-
-        foreach(array_keys($this->getHeaders()) as $key){
-
+    private function applyHeaders()
+    {
+        foreach (array_keys($this->getHeaders()) as $key) {
             $this->applyHeader($key);
-
         }
 
         return $this;
-
     }
 
     /**
@@ -97,102 +89,86 @@ class Flush{
      * @param bool $prepare
      * @return Flush
      */
-    public function data($data, $prepare = false){
-
+    public function data($data, $prepare = false)
+    {
         echo $data;
 
-        if($this->isPrepared){
-
+        if ($this->isPrepared) {
             $this->dump();
-
-        }
-        else if($prepare){
-
+        } else if($prepare) {
             $this->prepare();
-
         }
 
         return $this;
-
     }
 
     /**
      * @param bool $end_flush
      * @return Flush
      */
-    public function dump($end_flush = false){
-
-        if($end_flush){
-
+    public function dump($end_flush = false)
+    {
+        if ($end_flush) {
             while(@ob_end_flush());
-
         }
 
         @ob_flush();
         @flush();
 
         return $this;
-
     }
 
     /**
      * @return string
      */
-    public function getContentType(){
-
+    public function getContentType()
+    {
         return $this->getHeader(self::HEADER_KEY_CONTENT_TYPE);
-
     }
 
     /**
      * @param string $key 
      * @return string
      */
-    public function getHeader($key){
-
+    public function getHeader($key)
+    {
         $headers = $this->getHeaders();
         $value = '';
 
-        if(isset($headers[$key])){
-
+        if (isset($headers[$key])) {
             $value = $headers[$key];
-
         }
 
         return $value;
-
     }
 
     /**
      * @return array
      */
-    public function getHeaders(){
-
+    public function getHeaders()
+    {
         return $this->headers;
-
     }
 
     /**
      * @param string $key 
      * @return string
      */
-    private function getHeaderAndRemove($key){
-
+    private function getHeaderAndRemove($key)
+    {
         $header = $this->getHeader($key);
 
         $this->removeHeader($key);
 
         return $header;
-
     }
 
     /**
      * @return bool
      */
-    public function isLimboEnabled(){
-
+    public function isLimboEnabled()
+    {
         return $this->enableLimbo;
-
     }
 
     /**
@@ -200,28 +176,24 @@ class Flush{
      * @param bool $prepare
      * @return Flush
      */
-    public function json($data, $prepare = null){
-
-        if(is_null($prepare) && $this->isLimboEnabled()){
-
+    public function json($data, $prepare = null)
+    {
+        if (is_null($prepare) && $this->isLimboEnabled())
+        {
             $prepare = true;
-
         }
 
         return $this->data(json_encode($data), $prepare);
-
     }
 
     /**
      * @return Flush
      */
-    public function prepare(){
-
-        if($this->isLimboEnabled()){
-
+    public function prepare()
+    {
+        if ($this->isLimboEnabled()) {
             $this->addHeader(self::HEADER_KEY_CONTENT_LENGTH, ob_get_length());
             $this->addHeader(self::HEADER_KEY_CONNECTION, 'close');
-
         }
 
         $this->applyHeaders();
@@ -229,10 +201,8 @@ class Flush{
         ignore_user_abort($this->isLimboEnabled());
         set_time_limit(0);
         
-        if(session_id()){
-
+        if (session_id()) {
             session_write_close();
-
         }
 
         ini_set('implicit_flush', false);
@@ -244,57 +214,49 @@ class Flush{
         $this->isPrepared = (bool) $this->dump(true);
 
         return $this;
-
     }
 
     /**
      * @param string $key 
      * @return Flush
      */
-    public function removeHeader($key){
-
+    public function removeHeader($key)
+    {
         unset($this->headers[$key]);
 
         return $this;
-
     }
 
     /**
      * @param string $value
      * @return Flush
      */
-    public function setContentType($value){
-
+    public function setContentType($value)
+    {
         return $this->addHeader(self::HEADER_KEY_CONTENT_TYPE, $value);
-
     }
 
     /**
      * @param bool $enabled
      * @return Flush
      */
-    public function setLimboEnabled($enabled){
-
+    public function setLimboEnabled($enabled)
+    {
         $this->enableLimbo = $enabled;
 
         return $this;
-
     }
 
     /**
      * @param bool $send
      * @return bool
      */
-    public function signal($send = true){
-
-        if($send){
-
+    public function signal($send = true)
+    {
+        if ($send) {
             $this->data(chr(32));
-
         }
 
         return $send && !connection_aborted();
-
     }
-
 }
